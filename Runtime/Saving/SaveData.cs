388cda5e0049034bb210b17e45cb68ffbb4bbc48
Kowl1307 <kilian.sinke@gmx.de>
2025-10-05 16:51:38 +0200

    added save system and main thread dispatcher

---
+++ Runtime/Saving/SaveData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Saving
+{
+    [Serializable]
+    public abstract class SaveData
+    {
+        public void Save(string fileName)
+        {
+            SaveSystem.SaveData(this, fileName);
+        }
+
+        public virtual T Load<T>(string fileName) where T : SaveData
+        {
+            return SaveSystem.LoadData<T>(fileName);
+        }
+    }
+}
