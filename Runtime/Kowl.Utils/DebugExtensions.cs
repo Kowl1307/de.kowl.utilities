34da13e5da156de492bab0d7fe73aab10ddaf5ee
Kilian <Kilian.Sinke@gmx.de>
2025-12-09 19:27:11 +0100

    added debug extension

---
+++ Runtime/Kowl.Utils/DebugExtensions.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Kowl.Utils
+{
+    public static class DebugExtensions
+    {
+        [System.Diagnostics.Conditional("UNITY_EDITOR")]
+        [System.Diagnostics.Conditional("DEVELOPMENT_BUILD")]
+        public static void Log(this MonoBehaviour obj, object message)
+        {
+            Debug.Log(message, obj);
+        }
+    }
+}
